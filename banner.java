a52dee10cc940f94340ddbec276af4ab0957fcfe
subhamagrawal23 <subhamagrawal2323@gmail.com>
2026-02-17 15:54:45 +0530

    Initial commit: add banner

---
+++ banner.java
@@ -0,0 +1,13 @@
+public class banner{
+ public static void main(String[] args){
+     System.out.println("  ***         ***       ******        *****     ");
+     System.out.println("**    **    **    **    **    **    **          ");
+     System.out.println("**    **    **    **    **     **  **           ");
+     System.out.println("**    **    **    **    **    **    **          ");
+     System.out.println("**    **    **    **    ******        ***       ");
+     System.out.println("**    **    **    **    **               **     ");
+     System.out.println("**    **    **    **    **                **    ");
+     System.out.println("**    **    **    **    **               **     ");
+     System.out.println("  ***         ***       **          *****       ");
+    } 
+}
